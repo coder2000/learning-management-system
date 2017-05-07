5f329b0b35b7eeef199e2bab84b6b26cacdef26e
nsantiago2719 <nsantiago2719@gmail.com>
2017-05-07 16:15:33 +0800

    removed path on create repository link

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -9,4 +9,15 @@ FactoryGirl.define do
     gender                "male"
     role                  :student
   end
+
+  factory :clean_instructor, class: "User" do
+    email                 Faker::Internet.email
+    password              "secret"
+    password_confirmation "secret"
+    fname                 Faker::Name.first_name
+    mname                 Faker::Name.last_name
+    lname                 Faker::Name.last_name
+    gender                "male"
+    role                  :instructor
+  end
 end
