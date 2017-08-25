7863d04cfd69b98bda32bcb6c0da1d3622b86ab8
Dieter Lunn <coder2000@gmail.com>
2017-08-24 21:40:44 -0500

    reduce complexity of extra helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,26 @@
 # Application helper
 module ApplicationHelper
-  def has_extra(quiz, homework, resources, post_link)
+  def extra?(quiz, homework, resources, post_link)
     content_tag :div, class: 'extra' do
       concat 'Has: ' if quiz || homework || resources
-      concat content_tag :div, 'Quiz', class: 'ui label' if quiz
-      concat content_tag :div, 'Homework', class: 'ui label' if homework
-      if resources
-        concat content_tag :div, 'Downloadable Resources', class: 'ui label'
-      end
+      quiz_tag(quiz)
+      homework_tag(homework)
+      resources_tag(resources)
       concat content_tag :a, 'View Post', class: 'ui button right floated',
                                           href: post_link
     end
+  end
+
+  def quiz_tag(quiz)
+    concat content_tag :div, 'Quiz', class: 'ui label' if quiz
+  end
+
+  def homework_tag(homework)
+    concat content_tag :div, 'Homework', class: 'ui label' if homework
+  end
+
+  def resources_tag(resources)
+    concat content_tag :div, 'Downloadable Resources', class: 'ui label' if resources
   end
 
   def ctime_ago_in_words(time_str)
@@ -40,7 +50,7 @@ module ApplicationHelper
     if controller.controller_name == 'posts'
       render 'posts/post_right_sidebar'
     else
-      render 'right_sidebar'
+      render 'application/right_sidebar'
     end
   end
 
--- app/helpers/records_helper.rb
+++ app/helpers/records_helper.rb
@@ -1,17 +1,18 @@
 module RecordsHelper
-  def link_to_record(record, method=:get, &block)
+  def link_to_record(record, method = :get)
     case method
 
     when :delete
-      link_to repository_record_path(record.repository.id, record.id), data: { confirm: "Are you sure you want to delete this video", method: method } do
+      link_to repository_record_path(record.repository.id, record.id),
+              data: { confirm: 'Are you sure you want to delete this video',
+                      method: method } do
         yield
       end
-    else:get
-      link_to repository_record_path(record.repository.id, record.id), target: "_blank" do
+    else
+      link_to repository_record_path(record.repository.id, record.id),
+              target: '_blank' do
         yield
       end
     end
-
   end
-
 end
