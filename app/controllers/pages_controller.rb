8dbba3e7a2e20309e0469c7ade35e2a664ab1681
Dieter Lunn <coder2000@gmail.com>
2017-08-30 16:41:45 -0500

    Update sub menu and repository show

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,11 +3,11 @@ class PagesController < ApplicationController
   layout 'application', except: [:login]
   before_action :check_if_logged_in, only: [:login]
   skip_before_action :require_login, only: [:login]
-  
+
   def login; end
 
   def index
-    @posts =  current_user.member_of.collect(&:posts).flatten
+    @posts = current_user.member_of.collect(&:posts).flatten
     @posts = @posts.sort { |x, y| y.created_at <=> x.created_at }
     @posts = Kaminari.paginate_array(@posts).page(params[:page]).per(6)
 
